72138defb6032e0c23725795ec630f63e7dca48d
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2021-09-14 12:37:01 +0530

    database con added using pymongo

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+import csv
+
+from flask import Flask, render_template
+import os, pymongo, urllib
 from src import *
 
 app = Flask(__name__)
 
+@app.route('/template')
+def template_page():
+    return render_template('index.html')
+
+
 @app.route('/')
 def home_page():
+    try:
+        username=urllib.parse.quote_plus(os.environ['username'])
+        password=urllib.parse.quote_plus(os.environ['password'])
+        uri=os.environ['mongo'].format(username,password)
+        client=pymongo.MongoClient(uri)
+        db=client[os.environ['database']]
+        collection=db[os.environ['collection1']]
+        data=collection.find_one()
+        print(data)
+    except Exception as e:
+        print(e)
+        print("no connection")
     return home_page_handler()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
